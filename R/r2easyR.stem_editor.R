7b437baf83e90a7c2c8e75566e02fb8f5fccdee3
JPSieg <jus841@psu.edu>
2020-09-25 22:53:08 -0400

    Fixed bu that occured when a helix started on the next nucleotide after another helix ended

--- R/r2easyR.stem_editor.R
+++ R/r2easyR.stem_editor.R
@@ -19,85 +19,112 @@ r2easyR.stem_editor = function(R2R.sto){
   stems <- 0
   R2R_LABEL <- 0
   ends_in_a_helix <- FALSE
+  next.N.start.stem <- FALSE
 
   open_labels <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M")
   close_labels <- c("N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 
+  length(SS_cons)
+
   for (i in 1:length(SS_cons)){
-    if (stems > 1){
-      if (open_pairs != 0){
-        if (SS_cons[i] == "<"){
-          open_pairs = open_pairs + 1
-          R2R_LABEL[i] = "."
-        }
-        if (SS_cons[i] == ">"){
-          open_pairs = open_pairs - 1
-          R2R_LABEL[i] = "."
-          if (open_pairs == 0){
-            if (stems %% 2 == 0){
-              if (i != length(SS_cons)){
-                R2R_LABEL[i] = close_labels[(stems - 1)]
-              }
-              if (i == length(SS_cons)){
-                ends_in_a_helix <- TRUE
+    if(next.N.start.stem) {
+      open_pairs = open_pairs + 1
+      stems = stems + 1
+      R2R_LABEL[i] = open_labels[(stems - 1)]
+      next.N.start.stem <- FALSE
+    } else {
+      if (stems > 1){
+        if (open_pairs != 0){
+          if (SS_cons[i] == "<"){
+            open_pairs = open_pairs + 1
+            R2R_LABEL[i] = "."
+          }
+          if (SS_cons[i] == ">"){
+            open_pairs = open_pairs - 1
+            R2R_LABEL[i] = "."
+            if (open_pairs == 0){
+              if (stems %% 2 == 0){
+                if (i != length(SS_cons)){
+                  R2R_LABEL[i] = close_labels[(stems - 1)]
+                }
+                if (SS_cons[(i+1)] == "<"){
+                  next.N.start.stem <- TRUE
+                }
+                if (i == length(SS_cons)){
+                  ends_in_a_helix <- TRUE
+                  next.N.start.stem <- FALSE
+                }
               }
             }
           }
-        }
-        if (SS_cons[i] == "."){
-          R2R_LABEL[i] = "."
-        }
-      }
-      if (open_pairs == 0){
-        if (SS_cons[i] == "<"){
-          stems = stems + 1
-          open_pairs = open_pairs + 1
-          if (stems %% 2 == 0){
-            R2R_LABEL[i] = open_labels[(stems - 1)]
+          if (SS_cons[i] == "."){
+            R2R_LABEL[i] = "."
           }
         }
-        if (SS_cons[i] == "."){
-          R2R_LABEL[i] = "."
+        if (open_pairs == 0){
+          if (SS_cons[i] == "<"){
+            stems = stems + 1
+            open_pairs = open_pairs + 1
+            if (stems %% 2 == 0){
+              R2R_LABEL[i] = open_labels[(stems - 1)]
+            }else{
+              R2R_LABEL[i] = "."
+            }
+          }
+          if (SS_cons[i] == "."){
+            R2R_LABEL[i] = "."
+          }
+        }
+      }
+      if (stems == 1){
+        if (open_pairs != 0){
+          if (SS_cons[i] == "<"){
+            open_pairs = open_pairs + 1
+            R2R_LABEL[i] = "."
+          }
+          if (SS_cons[i] == ">"){
+            open_pairs = open_pairs - 1
+            R2R_LABEL[i] = "."
+            if (open_pairs == 0){
+              if (SS_cons[(i+1)] == "<"){
+                next.N.start.stem <- TRUE
+              }
+            }
+          }
+          if (SS_cons[i] == "."){
+            R2R_LABEL[i] = "."
+          }
+        }
+        if (open_pairs == 0){
+          if (SS_cons[i] == "<"){
+            stems = stems + 1
+            open_pairs = open_pairs + 1
+            R2R_LABEL[i] = open_labels[(stems - 1)]
+          }
+          if (SS_cons[i] == "."){
+            R2R_LABEL[i] = "."
+          }
+        }
+      }
+      if (stems == 0){
+        if (open_pairs == 0){
+          if (SS_cons[i] == "<"){
+            stems = stems + 1
+            open_pairs = open_pairs + 1
+            R2R_LABEL[i] = "."
+          }
+          if (SS_cons[i] != "<"){
+            R2R_LABEL[i] = "."
+          }
         }
       }
     }
-    if (stems == 1){
-      if (open_pairs != 0){
-        if (SS_cons[i] == "<"){
-          open_pairs = open_pairs + 1
-          R2R_LABEL[i] = "."
-        }
-        if (SS_cons[i] == ">"){
-          open_pairs = open_pairs - 1
-          R2R_LABEL[i] = "."
-        }
-        if (SS_cons[i] == "."){
-          R2R_LABEL[i] = "."
-        }
-      }
-      if (open_pairs == 0){
-        if (SS_cons[i] == "<"){
-          stems = stems + 1
-          open_pairs = open_pairs + 1
-          R2R_LABEL[i] = open_labels[(stems - 1)]
-        }
-        if (SS_cons[i] == "."){
-          R2R_LABEL[i] = "."
-        }
-      }
-    }
-    if (stems == 0){
-      if (open_pairs == 0){
-        if (SS_cons[i] == "<"){
-          stems = stems + 1
-          open_pairs = open_pairs + 1
-          R2R_LABEL[i] = "."
-        }
-        if (SS_cons[i] != "<"){
-          R2R_LABEL[i] = "."
-        }
-      }
-    }
+
+    print(paste("i =", i))
+    print(paste("open pairs", open_pairs))
+    print(paste("stems", stems))
+    print(next.N.start.stem)
+    print(R2R_LABEL)
   }
 
   lines[4] <- paste("#=GC R2R_LABEL", gsub(", ", "", toString(R2R_LABEL)), sep = "\t")
