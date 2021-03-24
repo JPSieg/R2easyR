49e7203decd7315b4bbf652b3f4719414147770d
forstmeier-pc <pcf5065@psu.edu>
2021-03-24 19:50:33 -0400

    Added pk_finder and updated pknot_drawer
    Added pk_finder, a function that identifies pseudoknots in a .ct file, and the associated documentation. Updated pk_drawer to accept pk_finder's output.

--- R/r2easyR.pknot_drawer.R
+++ R/r2easyR.pknot_drawer.R
@@ -8,86 +8,94 @@
 #' @export
 r2easyR.pknot_drawer = function(R2R.sto,
                                pknot){
-  con <- file(R2R.sto)
-  lines <- readLines(con)
-
-  lines <- lines[1:(length(lines)-1)]
-
-  #index of labels to use in the R2R label line
-
-  label.index <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "x", "t", "u", "v", "w", "x", "y", "z")
-
-  label.line <- strsplit(strsplit(lines[4], split = "\t")[[1]][2], split = "")[[1]]
-
-  #Filters out labels that are already present in the label line (in case another pknot is already specified)
-
-  if (length(which(label.index == intersect(label.index, label.line))) != 0){
-    label.index <- label.index[-which(label.index == intersect(label.index, label.line))]
+  if (class(pknot) != "list"){
+    pknot = list(pknot)
   }
 
-  #add labels to the label line while keeping track of what labels have been added and not using the same labels for non contiguous nucleotides
+  for (l in 1:length(pknot)){
 
-  firstN <- TRUE
-  label.index.pos <- 1
-  unique.labels <- c()
-  gaps <- c()
+    con <- file(R2R.sto)
+    lines <- readLines(con)
 
-  for (i in 1:length(pknot)){
-    if (firstN){
-      label.line[pknot[i]] <- label.index[label.index.pos]
-      firstN <- FALSE
-      unique.labels <- c(unique.labels, label.index[label.index.pos])
-    }else{
-      if (pknot[i]-1 == pknot[i -1]){
-        label.line[pknot[i]] <- label.index[label.index.pos]
-      }else{
-        label.index.pos <- label.index.pos + 1
-        label.line[pknot[i]] <- label.index[label.index.pos]
+    lines <- lines[1:(length(lines)-1)]
+
+    #index of labels to use in the R2R label line
+
+    label.index <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "x", "t", "u", "v", "w", "x", "y", "z")
+
+    label.line <- strsplit(strsplit(lines[4], split = "\t")[[1]][2], split = "")[[1]]
+
+    #Filters out labels that are already present in the label line (in case another pknot is already specified)
+
+    if (length(which(label.index == intersect(label.index, label.line))) != 0){
+      label.index <- label.index[-which(label.index == intersect(label.index, label.line))]
+    }
+
+    #add labels to the label line while keeping track of what labels have been added and not using the same labels for non contiguous nucleotides
+
+    firstN <- TRUE
+    label.index.pos <- 1
+    unique.labels <- c()
+    gaps <- c()
+
+    for (i in 1:length(pknot[[l]])){
+      if (firstN){
+        label.line[pknot[[l]][i]] <- label.index[label.index.pos]
+        firstN <- FALSE
         unique.labels <- c(unique.labels, label.index[label.index.pos])
-        gaps <- c(gaps, i)
+      }else{
+        if (pknot[[l]][i]-1 == pknot[[l]][i -1]){
+          label.line[pknot[[l]][i]] <- label.index[label.index.pos]
+        }else{
+          label.index.pos <- label.index.pos + 1
+          label.line[pknot[[l]][i]] <- label.index[label.index.pos]
+          unique.labels <- c(unique.labels, label.index[label.index.pos])
+          gaps <- c(gaps, i)
+        }
       }
     }
+
+    #Add an edited label line
+
+    lines[4] <- paste("#=GC R2R_LABEL\t", gsub(", ", "", toString(label.line)), sep = "")
+
+    #Counts how many pknots are already in the label line
+
+    n.pknot = (length(c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "x", "t", "u", "v", "w", "x", "y", "z")) - length(label.index))/2 + 1
+
+    #Find middle of contiguous pknot regions for label. Use gaps, eg where there are breaks in contiguous pknot sequences
+
+    pknot.label.index <- c()
+
+    if (length(gaps) == 1){
+      pknot.label.index <- c(pknot.label.index, ceiling(mean(pknot[[l]][1:(gaps-1)])), ceiling(mean(pknot[[l]][(gaps):length(pknot[[l]])])))
+    }else{
+      #Don't know how to do this yet. I think I need to use a for loop to recursively specify mean pknot regions.
+      print("You are recieving this error message because the creator did not have the time to figure this out when he wrote this code. please email jpsieg225@gmail.com or jus841@psu.edu if you encounter this error message")
+    }
+
+    #Generate Peter's label commands
+
+    pknot.label.line <- rep(".", length(label.line))
+
+    new.lines <- c()
+
+    for (i in 1:length(unique.labels)){
+      new.lines <- c(new.lines, paste('#=GF R2R outline_nuc', unique.labels[i]))
+      new.lines <- c(new.lines, paste('#=GF R2R outline_nuc', unique.labels[i]))
+      new.lines <- c(new.lines, paste('#=GF R2R tick_label pk:', unique.labels[i], ' pk', n.pknot, sep = ""))
+      pknot.label.line[pknot.label.index[i]] <- unique.labels[i]
+    }
+
+    new.lines <- c(paste('#=GC R2R_XLABEL_pk\t', gsub(", ", "", toString(pknot.label.line)), sep = ""), new.lines)
+
+    lines <- c(lines, new.lines, "//")
+
+    print(lines[4])
+    print(new.lines)
+
+    writeLines(lines, con)
+    close(con)
   }
 
-  #Add an edited label line
-
-  lines[4] <- paste("#=GC R2R_LABEL\t", gsub(", ", "", toString(label.line)), sep = "")
-
-  #Counts how many pknots are already in the label line
-
-  n.pknot = (length(c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "x", "t", "u", "v", "w", "x", "y", "z")) - length(label.index))/2 + 1
-
-  #Find middle of contiguous pknot regions for label. Use gaps, eg where there are breaks in contiguous pknot sequences
-
-  pknot.label.index <- c()
-
-  if (length(gaps) == 1){
-    pknot.label.index <- c(pknot.label.index, ceiling(mean(pknot[1:(gaps-1)])), ceiling(mean(pknot[(gaps):length(pknot)])))
-  }else{
-    #Don't know how to do this yet. I think I need to use a for loop to recursively specify mean pknot regions.
-    print("You are recieving this error message because the creator did not have the time to figure this out when he wrote this code. please email jpsieg225@gmail.com or jus841@psu.edu if you encounter this error message")
-  }
-
-  #Generate Peter's label commands
-
-  pknot.label.line <- rep(".", length(label.line))
-
-  new.lines <- c()
-
-  for (i in 1:length(unique.labels)){
-    new.lines <- c(new.lines, paste('#=GF R2R outline_nuc', unique.labels[i]))
-    new.lines <- c(new.lines, paste('#=GF R2R outline_nuc', unique.labels[i]))
-    new.lines <- c(new.lines, paste('#=GF R2R tick_label pk:', unique.labels[i], ' pk', n.pknot, sep = ""))
-    pknot.label.line[pknot.label.index[i]] <- unique.labels[i]
-  }
-
-  new.lines <- c(paste('#=GC R2R_XLABEL_pk\t', gsub(", ", "", toString(pknot.label.line)), sep = ""), new.lines)
-
-  lines <- c(lines, new.lines, "//")
-
-  print(lines[4])
-  print(new.lines)
-
-  writeLines(lines, con)
-  close(con)
 }
